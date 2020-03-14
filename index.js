dca458b46600112fa27e481c9103e8e95c3793ed
GoTo15 <github email address>
2020-03-14 12:02:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-let totalBatteries = batteryBatches.reduce(function(total = 0, battery) {
+let totalBatteries = batteryBatches.reduce(function(total, battery) {
   return (total += battery);
 });
